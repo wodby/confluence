0a0ed9fcaf4393c0bafca0aeba0effdf30b2045f
Chingis Sandanov <chingis@wodby.com>
2017-04-21 14:24:48 +0700

    Support JIRA certificate

--- 6.0/docker-confluence-entrypoint.sh
+++ 6.0/docker-confluence-entrypoint.sh
@@ -8,4 +8,17 @@ fi
 
 chown daemon:daemon /var/atlassian/confluence
 
+if [[ -n "${JIRA_HOST}" ]]; then
+    jre="/usr/lib/jvm/java-8-openjdk-amd64/jre"
+
+    if [[ -z "${JIRA_PORT}" ]]; then
+        JIRA_PORT=443
+    fi
+
+    url="${JIRA_HOST}:${JIRA_PORT}"
+    openssl s_client -connect "${url}"  < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/cert.pem
+    "${jre}/bin/keytool" -import -alias jira -trustcacerts -keystore "${jre}/lib/security/cacerts" -file /tmp/cert.pem
+    rm /tmp/cert.pem
+fi
+
 gosu daemon /docker-entrypoint.sh "${@}"
