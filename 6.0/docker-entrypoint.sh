597a779deb0cdb3f96f57da29768a860144c68cd
Chingis Sandanov <chingis@wodby.com>
2017-05-24 15:19:26 +0700

    Rebase to Oracle Java

--- 6.0/docker-entrypoint.sh
+++ 6.0/docker-entrypoint.sh
@@ -6,21 +6,25 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
+importJiraKeys() {
+    if [[ -z "${JIRA_PORT}" ]]; then
+        JIRA_PORT=443
+    fi
+
+    url="${JIRA_HOST}:${JIRA_PORT}"
+    openssl s_client -connect "${url}"  < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/cert.pem
+    "${JAVA_HOME}/jre/bin/keytool" -import -alias jira -trustcacerts -keystore "${JAVA_HOME}/jre/lib/security/cacerts" \
+        -file /tmp/cert.pem -storepass changeit -noprompt
+    rm /tmp/cert.pem
+}
+
 if [[ "${1}" == 'make' ]]; then
     su-exec daemon "${@}" -f /usr/local/bin/actions.mk
 else
     chown daemon:daemon /var/atlassian/confluence
 
     if [[ -n "${JIRA_HOST}" ]]; then
-        if [[ -z "${JIRA_PORT}" ]]; then
-            JIRA_PORT=443
-        fi
-
-        url="${JIRA_HOST}:${JIRA_PORT}"
-        openssl s_client -connect "${url}"  < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > /tmp/cert.pem
-        "${JAVA_HOME}/jre/bin/keytool" -import -alias jira -trustcacerts -keystore "${JAVA_HOME}/jre/lib/security/cacerts" \
-            -file /tmp/cert.pem -storepass changeit -noprompt
-        rm /tmp/cert.pem
+        importJiraKeys
     fi
 
     su-exec daemon "${@}"
